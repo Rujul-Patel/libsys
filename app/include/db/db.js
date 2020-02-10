48ac9db7a76317ffb506f22274278585e47bcc76
rujul <rujulypatel@gmail.com>
2020-02-10 22:32:07 +0530

    first commit

---
+++ app/include/db/db.js
@@ -0,0 +1,47 @@
+
+function connectDb()
+{
+
+var mysql = require('mysql');
+
+//Credentials to access your database
+var connection = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'test',
+  database:'library'
+});
+  connection.connect(function(err){
+    //in case of error
+    if(err){
+      console.log(err.code);
+      console.log(err.fatal);
+    }
+    console.log('Connected to MySQL Database')
+  });
+
+
+}
+
+
+function dbQuery(q)
+{
+  connection.query(q,function(err,rows,fields){
+    if(err){
+      console.log("Query Failed!! Error");
+      console.log(err);
+      return;
+    }
+
+    console.log('Query Successfull');
+  });
+}
+
+
+function closeDb()
+{
+  connection.end(function(){
+    //connection closed
+    console.log('Connection Closed');
+  })
+}
